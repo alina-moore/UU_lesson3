952b7c203c7a8aebc7476095cb213d902fce0a0f
Alina A <antolina94@gmail.com>
2024-05-19 23:30:48 +0300

    Initial commit

---
+++ homework3.py
@@ -0,0 +1,11 @@
+name = 'Алина'
+print('Name: ' + name)
+
+age = 29
+print('Age: ' + str(age))
+
+age = age + 1
+print('New age: ' + str(age))
+
+is_student = True
+print('Is student: ' + str(is_student))
